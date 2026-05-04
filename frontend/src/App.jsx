edfe4c3250d306d50a07703cdaf12d72f89ed808
subannaeemgithub <subannaeem2003@gmail.com>
2026-05-04 15:25:59 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
+import AdminDashboard from "./pages/admin/AdminDashboard";
 import RoutesApp from "./routes/routesApp"; // 👈 import your routes
 
 function App() {
+  return   <RoutesApp />; // 👈 use it here
 }
 
 export default App;
